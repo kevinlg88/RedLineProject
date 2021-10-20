28e48c86e95c5e843743da0d4d183fe3c6bbced6
Kevin Grandez <kevinlg08@gmail.com>
2021-10-20 15:45:06 -0300

    Alteracoes

--- Assets/GameAssets/Scripts/TimeCounter.cs
+++ Assets/GameAssets/Scripts/TimeCounter.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class TimeCounter : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public TextMeshProUGUI gameTimerText;
+    float gameTimer = 0f;
     void Update()
     {
-        
+        gameTimer += Time.deltaTime;
+
+        int seconds = (int)(gameTimer%60);
+        int minutes = (int)(gameTimer / 60) % 60;
+        int hours = (int)(gameTimer/3600) % 24;
+
+        string timerString = string.Format("{0:00}:{1:00}:{2:00}",hours,minutes,seconds);
+
+        gameTimerText.text = timerString;
     }
 }
