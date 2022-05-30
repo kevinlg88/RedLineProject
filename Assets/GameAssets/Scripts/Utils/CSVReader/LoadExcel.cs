dcaf5d1bc61ba2c1c1c63d0018fd645c0d232611
Kevin Grandez <kevinlg08@gmail.com>
2022-05-30 18:58:50 -0300

    fix

--- Assets/GameAssets/Scripts/Utils/CSVReader/LoadExcel.cs
+++ Assets/GameAssets/Scripts/Utils/CSVReader/LoadExcel.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class LoadExcel : MonoBehaviour
 {
     public Item blankItem;
-    public List<Item> itemDatabase = new List
     // Start is called before the first frame update
     void Start()
     {
